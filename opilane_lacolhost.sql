49bcb6faed17e8550312a846148b331f36e7da70
Anastasiia Radasheva <eha20082@gmail.com>
2025-02-04 15:44:10 +0200

    Update opilane_lacolhost.sql

--- opilane_lacolhost.sql
+++ opilane_lacolhost.sql
@@ -26,3 +26,68 @@ values(
 delete from opilane Where opilaneid=2
 UPDATE opilane SET aadress='Tartu'
 WHERE opilane=3
+----------------------------------------------------------
+
+
+
+SELECT * FROM opilane
+
+CREATE PROCEDURE opilaneUuendus
+    @opilaneID INT,
+    @p_eesnimi VARCHAR(50),
+    @p_perenimi VARCHAR(50),
+    @p_synniaeg DATE,
+    @p_stip DECIMAL(5,2),
+    @p_keskmine_hinne DECIMAL(3,2)
+AS
+BEGIN
+    UPDATE opilane
+    SET eesnimi = @p_eesnimi,
+        perenimi = @p_perenimi,
+        synniaeg = @p_synniaeg,
+        stip = @p_stip,
+        keskmine_hinne = @p_keskmine_hinne
+    WHERE opilaneID = @opilaneID;
+END;
+SELECT * FROM opilane
+
+
+CREATE PROCEDURE opilaneSisesta
+    @p_eesnimi VARCHAR(50),
+    @p_perenimi VARCHAR(50),
+    @p_synniaeg DATE,
+    @p_stip DECIMAL(5,2),
+    @p_keskmine_hinne DECIMAL(3,2)
+AS
+BEGIN
+    INSERT INTO opilane (
+        eesnimi,
+        perenimi,
+        synniaeg,
+        stip,
+        keskmine_hinne)
+    VALUES
+    (@p_eesnimi,
+     @p_perenimi,
+     @p_synniaeg,
+     @p_stip,
+     @p_keskmine_hinne);
+END;
+SELECT * FROM opilane
+
+CREATE PROCEDURE opilaneVali
+    @p_opilaneID INT
+AS
+BEGIN
+    SELECT * 
+    FROM opilane 
+    WHERE opilaneID = @p_opilaneID;
+END;
+CREATE PROCEDURE opilaneKustuta
+    @p_opilaneID INT
+AS
+BEGIN
+    DELETE FROM opilane 
+    WHERE opilaneID = @p_opilaneID;
+END;
+SELECT * FROM opilane
